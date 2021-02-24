b2413bb7807bca551c47851e776a40aaabde78dc
damirqa <damirshaniyazov@gmail.com>
2021-02-24 12:17:41 +0500

    added crud of offer

---
+++ models/OfferSearch.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace app\models;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use app\models\Offer;
+
+/**
+ * OfferSearch represents the model behind the search form of `app\models\Offer`.
+ */
+class OfferSearch extends Offer
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'id_project', 'performer_id'], 'integer'],
+            [['bid'], 'number'],
+            [['date', 'scheduled_time_performer', 'text'], 'safe'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Offer::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'id_project' => $this->id_project,
+            'performer_id' => $this->performer_id,
+            'bid' => $this->bid,
+            'date' => $this->date,
+            'scheduled_time_performer' => $this->scheduled_time_performer,
+        ]);
+
+        $query->andFilterWhere(['like', 'text', $this->text]);
+
+        return $dataProvider;
+    }
+}
