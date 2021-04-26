947fc3d1efd554c1b3fbab93152a1da409812d5e
damirqa <damirshaniyazov@gmail.com>
2021-04-26 22:29:29 +0500

    minor fix

--- views/offer/view.php
+++ views/offer/view.php
@@ -6,7 +6,6 @@ use yii\widgets\DetailView;
 /* @var $this yii\web\View */
 /* @var $model app\models\Offer */
 
-$this->title = $model->id;
 $this->params['breadcrumbs'][] = ['label' => 'Offers', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
@@ -21,7 +20,7 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= $model->text ?>
     </div>
     <div class="offer-view-data">Дата выполнения: <?= date_format(new DateTime($model->date), "d.m.Y") ?></div>
-    <div class="offer-view-data">Цена: <?= $model->bid?></div>
+    <div class="offer-view-data">Цена: <?= $model->bid?> руб.</div>
     <div class="offer-view-data offer-control-buttons">
         <?= Html::a('Обновить', ['/offer/update', 'id' => $model->id], ['class' => 'a-btn']) ?>
         <?= Html::a('Удалить', ['/offer/delete', 'id' => $model->id], [
@@ -32,26 +31,5 @@ $this->params['breadcrumbs'][] = $this->title;
             ],
         ]) ?>
     </div>
-
-
-
-
-<!--    --><?//= DetailView::widget([
-//        'model' => $model,
-//        'attributes' => [
-//            'id',
-//            'project_id',
-//            'performer_id',
-//            'bid',
-//            'date',
-//            'scheduled_time_performer',
-//            'text',
-//        ],
-//    ]) ?>
-
-    <p>
-
-    </p>
-
 </div>
 
