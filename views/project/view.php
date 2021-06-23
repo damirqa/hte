af2db16e255913483f27a3d866b3164e2bdd8685
damirqa <damirshaniyazov@gmail.com>
2021-06-24 02:10:25 +0500

    delete offer info

--- views/project/view.php
+++ views/project/view.php
@@ -79,43 +79,9 @@ $this->params['breadcrumbs'][] = $this->title;
 
     </div>
 
-    <?php
-        if (is_null($offer) && $count == 0 && Yii::$app->getUser()->getId() == $model->customer_id) {
-            ?>
-                <div class="project-offers-count project-offers-zero">
-                    У вас нет предложений
-                </div>
-            <?php
-        }
+    <?=
+        $this->render('@app/views/offer/_count.php', ['offer' => $offer, 'count' => $count]);
     ?>
-
-    <?php
-        if (is_null($offer) && $count != 0 && Yii::$app->getUser()->getId() == $model->customer_id) {
-            ?>
-                <div class="project-offers-count project-offers-not-accept">
-                    Количество предложений: <?= $count ?>, но Вы всё еще не приняли ни одного предложения... <a href="/project/offers?id=<?= $model->id ?>">Посмотреть?</a>
-                </div>
-            <?php
-        }
-    ?>
-
-    <?php
-        if (!is_null($offer) && Yii::$app->getUser()->getId() == $model->customer_id) {
-            ?>
-                <div class="project-offers-count project-offer-accept">
-                    <div class="project-offer-accept-data">Количество предложений: <?= $count ?>, хотите <a href="/project/offers?id=<?= $model->id ?>">посмотреть?</a></div>
-                    <div class="project-offer-accept-data">
-                        Вы приняли следующее предложение от <a href="/profile/view?id=<?= $performer->id ?>"><?= $performer->surname ?> <?= $performer->name ?></a>:
-                    </div>
-                    <div class="project-offer-accept-data">Описание: <?= $offer->text ?></div>
-                    <div class="project-offer-accept-data">Дата выполнения: <?= date_format(new DateTime($offer->date), 'd.m.Y') ?></div>
-                    <div class="project-offer-accept-data">Цена: <?= $offer->bid ?> ₽</div>
-                    <div class="project-offer-accept-data">Вы можете отказаться от данного предложения. <a href="#">Отказаться?</a></div>
-                </div>
-            <?php
-        }
-    ?>
-
 
         <?php
             if (Yii::$app->getUser()->getIsGuest()) {
