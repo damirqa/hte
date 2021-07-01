a6a2901f0a14c22ec33e99e0eebd41e0675ca75d
damirqa <damirshaniyazov@gmail.com>
2021-07-02 00:04:10 +0500

    added migr for create solution table

--- migrations/m210629_180342_create_solution.php
+++ migrations/m210629_180342_create_solution.php
@@ -15,6 +15,7 @@ class m210629_180342_create_solution extends Migration
         $this->createTable('solution', [
             'id' => $this->bigPrimaryKey(),
             'id_project' => $this->integer()->notNull(),
+            'id_performer' => $this->integer(),
             'date_create' => $this->date(),
             'date_change' => $this->date(),
             'status' => $this->string(),
@@ -28,6 +29,16 @@ class m210629_180342_create_solution extends Migration
             'project',
             'id',
             'CASCADE',
+            'CASCADE'
+        );
+
+        $this->addForeignKey(
+            'fk-performer-solution-id',
+            'solution',
+            'id_performer',
+            'profile',
+            'id',
+            'NO ACTION',
             'CASCADE'
         );
     }
